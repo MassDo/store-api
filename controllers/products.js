1153516e1a8f1d845b2b0050feddc2ed282b0696
massdo <dorian_massoulier@hotmail.com>
2021-08-27 23:11:07 +0200

    add filter/sort/pagination to the getProducts controller

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,45 @@
+const { query } = require('express')
 const Product = require('../models/product')
 
-const getProducts = async (req, res) =>{
-    const products = await Product.find({price:42})
-    res.status(200).json({products, nbHits: products.length})
-}
+
 const getProductsStatic = async (req, res) =>{
     const products = await Product.find({price:42})
     res.status(200).json({products, nbHits: products.length})
+}
+const getProducts = async (req, res) =>{
+    // filter for correct query fields
+    const {featured, company, name, sort, fields} = req.query
+    const queryObject = {}
+    if (featured){
+        queryObject.featured = featured === "true" ? true : false
+    }
+    if (company){
+        queryObject.company = company
+    }
+    if (name){
+        queryObject.name = {$regex:name, $options:'i'}
+    }
+    let searchResult = Product.find(queryObject)
+    // select fields
+    if (fields){
+        const selectList = fields.split(',')
+        searchResult = searchResult.select(selectList)
+    }
+    // sort fields
+    if(sort){
+        const sortStr = sort.split(',').join(' ')
+        searchResult = searchResult.sort(sortStr)
+    }else{
+        searchResult = searchResult.sort('createdAt')
+    }
+    // pagination
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit 
+    searchResult = searchResult.skip(skip).limit(limit)
+    // fetch database
+    const products = await searchResult
+    res.status(200).json({nbHits: products.length, products })
 }
 module.exports = {
     getProducts,
